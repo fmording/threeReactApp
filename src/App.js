38c958f3aa257f8de7cf75667b6853e51f2dac08
fmording <fmo@hotmail.no>
2021-04-14 13:15:30 +0200

    Functioning threejs scene is added. non-responsive but it renders, with start/stop controls for the cube animation in example.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import * as THREE from 'three';
 
 function App() {
+  const { useRef, useEffect, useState } = React;
+  const mount = useRef(null);
+  const [isAnimating, setAnimating] = useState(true);
+  const controls = useRef(null);
+
+  useEffect(() => {
+    let width = mount.current.clientWidth;
+    let height = mount.current.clientHeight;
+    let frameId;
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
+    const cube = new THREE.Mesh(geometry, material);
+
+    camera.position.z = 4;
+    scene.add(cube);
+    renderer.setClearColor('#000000');
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const renderScene = () => {
+      renderer.render(scene, camera);
+    };
+
+    const handleResize = () => {
+      width = mount.current.clientWidth;
+      height = mount.current.clientHeight;
+      renderer.setSize(width, height);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+      renderScene();
+    };
+
+    const animate = () => {
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderScene();
+      frameId = window.requestAnimationFrame(animate);
+    };
+
+    const start = () => {
+      if (!frameId) {
+        frameId = requestAnimationFrame(animate);
+      }
+    };
+
+    const stop = () => {
+      cancelAnimationFrame(frameId);
+      frameId = null;
+    };
+
+    mount.current.appendChild(renderer.domElement);
+    window.addEventListener('resize', handleResize);
+    start();
+
+    controls.current = { start, stop };
+
+    return () => {
+      stop();
+      window.removeEventListener('resize', handleResize);
+
+      scene.remove(cube);
+      geometry.dispose();
+      material.dispose();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isAnimating) {
+      controls.current.start();
+    } else {
+      controls.current.stop();
+    }
+  }, [isAnimating]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      ref={mount}
+      onClick={() => setAnimating(!isAnimating)}
+      className="tryDiv"
+    ></div>
   );
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
